69941678a83183356c3a294bf0be5c40caa92b91
Roman Simecek <roman@good2go.ch>
2014-06-23 15:51:17 +0200

    Skip active_record so that the dummy app works without a database.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails/all"
+require "active_model/railtie"
+# require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 
